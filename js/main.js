ff01579971643e1e900a01f1b448bba7d69c41b5
DanieleBernassola <danielebernassola02@gmail.com>
2024-05-14 15:50:37 +0200

    [UPD] Completed milestone 0

--- js/main.js
+++ js/main.js
@@ -1,2 +1,36 @@
 'use strict';
 
+// Creazione singoli oggetti persona
+const persona1 = {
+    firstName: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    image: 'img/wayne-barnett-founder-ceo.jpg',
+}
+const persona2 = {
+    firstName: 'Angela Caroll',
+    role: 'Chief Editor',
+    image: 'img/angela-caroll-chief-editor.jpg',
+}
+const persona3 = {
+    firstName: 'Walter Gordon',
+    role: 'Office Manager',
+    image: 'img/walter-gordon-office-manager.jpg',
+}
+const persona4 = {
+    firstName: 'Angela Lopez',
+    role: 'Social Media Manager',
+    image: 'img/angela-lopez-social-media-manager.jpg',
+}
+const persona5 = {
+    firstName: 'Scott Estrada',
+    role: 'Developer',
+    image: 'img/scott-estrada-developer.jpg',
+}
+const persona6 = {
+    firstName: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    image: 'img/barbara-ramos-graphic-designer.jpg',
+}
+
+// Creazione array persone
+const persone = [persona1, persona2, persona3, persona4, persona5, persona6];
