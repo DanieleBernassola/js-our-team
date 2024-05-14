7c5a69b58718615947a8c24063b60bd80aa346d9
DanieleBernassola <danielebernassola02@gmail.com>
2024-05-14 16:59:41 +0200

    [FIX] Various fixes in readme and js

--- README.md
+++ README.md
@@ -4,11 +4,10 @@ Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 MILESTONE 0:
 <!-- Creare l’array di oggetti con le informazioni fornite. -->
 
-- Creo un oggetto per ogni persona, ogni oggetto avrà:
+- Creo un array in cui inserisco gli oggetti, ogni oggetto avrà:
     -- nome
     -- ruolo
     -- immagine
-- Creo un array in cui inserisco gli oggetti
 
 MILESTONE 1:
 <!-- Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto -->
@@ -18,4 +17,10 @@ MILESTONE 1:
 MILESTONE 2:
 <!-- Stampare le stesse informazioni su DOM sottoforma di stringhe -->
 
-- Creo una ul in cui andrò ad appendere gli li con le relative informazioni
+- Creo una ul in cui andrò ad appendere i li con le relative informazioni
+
+BONUS 1:
+<!-- Trasformare la stringa foto in una immagine effettiva -->
+
+BONUS 2:
+<!-- Organizzare i singoli membri in card/schede -->
--- js/main.js
+++ js/main.js
@@ -1,43 +1,54 @@
 'use strict';
 
-// Creazione singoli oggetti persona
-const persona1 = {
-    firstName: 'Wayne Barnett',
-    role: 'Founder & CEO',
-    image: 'img/wayne-barnett-founder-ceo.jpg',
-}
-const persona2 = {
-    firstName: 'Angela Caroll',
-    role: 'Chief Editor',
-    image: 'img/angela-caroll-chief-editor.jpg',
-}
-const persona3 = {
-    firstName: 'Walter Gordon',
-    role: 'Office Manager',
-    image: 'img/walter-gordon-office-manager.jpg',
-}
-const persona4 = {
-    firstName: 'Angela Lopez',
-    role: 'Social Media Manager',
-    image: 'img/angela-lopez-social-media-manager.jpg',
-}
-const persona5 = {
-    firstName: 'Scott Estrada',
-    role: 'Developer',
-    image: 'img/scott-estrada-developer.jpg',
-}
-const persona6 = {
-    firstName: 'Barbara Ramos',
-    role: 'Graphic Designer',
-    image: 'img/barbara-ramos-graphic-designer.jpg',
-}
+const ul = document.querySelector('.list');
 
-// Creazione array persone
-const persone = [persona1, persona2, persona3, persona4, persona5, persona6];
+// Creazione array team con oggetti i membri del team
+const team = [
+    {
+        firstName: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'img/wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        firstName: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'img/angela-caroll-chief-editor.jpg',
+    },
+    {
+        firstName: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'img/walter-gordon-office-manager.jpg',
+    },
+    {
+        firstName: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'img/angela-lopez-social-media-manager.jpg',
+    },
+    {
+        firstName: 'Scott Estrada',
+        role: 'Developer',
+        image: 'img/scott-estrada-developer.jpg',
+    },
+    {
+        firstName: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'img/barbara-ramos-graphic-designer.jpg',
+    },
+]
 
-for (let key in persone){
+// Ciclo for in per visualizzare dati degli oggetti in console
+for (let key in team){
     // console.log(persone[key].firstName);
     // console.log(persone[key].role);
     // console.log(persone[key].image);
-    console.log(persone[key]);
+    console.log(team[key]);
 }
+
+// for (let i = 0; i < team.length; i++){
+//     const currentElement = team[i];
+//     const li = document.createElement('li');
+//     li.append(currentElement.firstName);
+//     li.append(currentElement.role);
+//     li.append(currentElement.image);
+//     ul.append(li);
+// }
