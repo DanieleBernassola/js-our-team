d46a2f5a134e33d787401140620830c7ed01ee4a
DanieleBernassola <danielebernassola02@gmail.com>
2024-05-14 17:11:16 +0200

    [UPD] Completed milestone 2

--- js/main.js
+++ js/main.js
@@ -7,32 +7,32 @@ const team = [
     {
         firstName: 'Wayne Barnett',
         role: 'Founder & CEO',
-        image: 'img/wayne-barnett-founder-ceo.jpg',
+        image: 'wayne-barnett-founder-ceo.jpg',
     },
     {
         firstName: 'Angela Caroll',
         role: 'Chief Editor',
-        image: 'img/angela-caroll-chief-editor.jpg',
+        image: 'angela-caroll-chief-editor.jpg',
     },
     {
         firstName: 'Walter Gordon',
         role: 'Office Manager',
-        image: 'img/walter-gordon-office-manager.jpg',
+        image: 'walter-gordon-office-manager.jpg',
     },
     {
         firstName: 'Angela Lopez',
         role: 'Social Media Manager',
-        image: 'img/angela-lopez-social-media-manager.jpg',
+        image: 'angela-lopez-social-media-manager.jpg',
     },
     {
         firstName: 'Scott Estrada',
         role: 'Developer',
-        image: 'img/scott-estrada-developer.jpg',
+        image: 'scott-estrada-developer.jpg',
     },
     {
         firstName: 'Barbara Ramos',
         role: 'Graphic Designer',
-        image: 'img/barbara-ramos-graphic-designer.jpg',
+        image: 'barbara-ramos-graphic-designer.jpg',
     },
 ]
 
@@ -44,11 +44,26 @@ for (let key in team){
     console.log(team[key]);
 }
 
-// for (let i = 0; i < team.length; i++){
-//     const currentElement = team[i];
-//     const li = document.createElement('li');
-//     li.append(currentElement.firstName);
-//     li.append(currentElement.role);
-//     li.append(currentElement.image);
-//     ul.append(li);
-// }
+// Ciclo for per gestire DOM
+for (let i = 0; i < team.length; i++){
+    const currentElement = team[i];
+    // Creazione list item
+    const li = document.createElement('li');
+
+    // Creazione img e append su li
+    const img = document.createElement('p');
+    img.append(currentElement.image);
+    li.append(img);
+
+    // Creazione h2 e append su li
+    const h2 = document.createElement('h2');
+    h2.append(currentElement.firstName);
+    li.append(h2);
+
+    // Creazione p(role) e append su li
+    const role = document.createElement('p');
+    role.append(currentElement.role);
+    li.append(role);
+    
+    ul.append(li);
+}
